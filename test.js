2a1b093412a6992fd250f559ce9b49a310a3784f
Jari Ylimäinen <jari@ylimainen.fi>
2015-08-03 21:06:03 +0300

    Initial commit

---
+++ test.js
@@ -0,0 +1,6 @@
+riot.tag('test', '<h3>{ test }</h3>', function(opts) {
+
+  this.test = 'This is a test';
+
+
+});
---
+++ test.tag
@@ -0,0 +1,7 @@
+<test>
+
+  <h3>{ test }</h3>
+
+  this.test = 'This is a test';
+
+</test>
